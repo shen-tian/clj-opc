d502b977f39f4d9e683ddf77c45e566edf595ab6
Shen Tian <biozinc@gmail.com>
2016-10-07 00:34:18 +0200

    First n-LEDs working

--- src/clj_opc/core.clj
+++ src/clj_opc/core.clj
@@ -2,7 +2,6 @@
   (:require
    [manifold.deferred :as d]
    [manifold.stream :as s]
-   [clojure.edn :as edn]
    [aleph.tcp :as tcp]
    [gloss.core :as gloss]
    [gloss.io :as io]))
@@ -11,13 +10,9 @@
   (gloss/compile-frame
    [:ubyte 
     :ubyte
-    :ubyte
-    :ubyte
-    :ubyte
-    :ubyte
-    :ubyte
-    :ubyte]))
+    (gloss/repeated :ubyte :prefix :uint16)]))
 
+;; Definitely don't need the duplex stream stuff here...
 (defn wrap-duplex-stream
   [protocol s]
   (let [out (s/stream)]
@@ -32,3 +27,14 @@
   [host port]
   (d/chain (tcp/client {:host host, :port port})
            #(wrap-duplex-stream opc-protocol %)))
+
+(defn make-opc-packed
+  [color n]
+  (merge [0 0] 
+         (reduce into (repeat n [(:red color)
+                                 (:green color)
+                                 (:blue color)]))))
+
+(defn show 
+  [client color n]
+  (s/put! client (make-opc-packed color n)))
