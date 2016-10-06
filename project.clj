1b45aa9555c3ac708fa31869323291c2411b5b4f
Shen Tian <biozinc@gmail.com>
2016-10-06 22:10:01 +0200

    Got aleph sample code to run

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clj-opc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Open Pixel Control Lib for Clojure"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [aleph "0.4.1"]
+                 [manifold "0.1.5"]
+                 [gloss "0.2.5"]])
--- src/clj_opc/core.clj
+++ src/clj_opc/core.clj
@@ -1,6 +1,47 @@
-(ns clj-opc.core)
+(ns clj-opc.core
+  (:require
+   [manifold.deferred :as d]
+   [manifold.stream :as s]
+   [clojure.edn :as edn]
+   [aleph.tcp :as tcp]
+   [gloss.core :as gloss]
+   [gloss.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def protocol
+  (gloss/compile-frame
+    (gloss/finite-frame :uint32
+      (gloss/string :utf-8))
+    pr-str
+    edn/read-string))
+
+
+(defn wrap-duplex-stream
+  [protocol s]
+  (let [out (s/stream)]
+    (s/connect
+      (s/map #(io/encode protocol %) out)
+      s)
+    (s/splice
+      out
+      (io/decode-stream s protocol))))
+
+(defn client
+  [host port]
+  (d/chain (tcp/client {:host host, :port port})
+           #(wrap-duplex-stream protocol %)))
+
+
+(defn start-server
+  [handler port]
+  (tcp/start-server
+    (fn [s info]
+      (handler (wrap-duplex-stream protocol s) info))
+    {:port port}))
+
+
+(defn fast-echo-handler
+  [f]
+  (fn [s info]
+    (s/connect
+      (s/map f s)
+      s)))
